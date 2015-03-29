c07ee0889bf1b8e488f2f56f32e18cfad3825089
unknown <rui.simoes@netvisao.pt>
2015-03-30 00:35:07 +0100

    Rule - Define o tipo de estecie (Planta, animal, fungo ou alga) Rule - Define quais as especies que sao parentes proximos.

--- core/rules.py
+++ core/rules.py
@@ -1,10 +1,39 @@
 
 from inferencerule import InferenceRule
 
-class ParentsRule(InferenceRule):
+class TypeRule(InferenceRule):
     def getqueries(self):
-        partner_enemy = [('?especie', 'is_type', 'Species')]
+        partner_enemy = [
+            ('?typeId', 'name', 'Kingdom'),
+            ('?kingdomId','type','?typeId'),
+            ('?kingdomId', 'name', '?kingdomName'),
+            ('?phylumId', 'belongs_to', '?kingdomId'),
+            ('?classId', 'belongs_to', '?phylumId'),
+            ('?orderId', 'belongs_to', '?classId'),
+            ('?familyId', 'belongs_to', '?orderId'),
+            ('?specieId', 'belongs_to', '?familyId')]
         return [partner_enemy]
 
-    def _maketriples(self, family, order, className, especie):
-        return [(especie, 'belongs_to_class', className)]
+    def _maketriples(self, typeId, kingdomId, kingdomName, phylumId, classId, orderId, familyId, specieId):
+        if (kingdomName == 'Fungi'):
+            kingdomName = 'Fungo'
+        elif (kingdomName == 'Animalia'):
+            kingdomName = 'Animal'
+        elif (kingdomName == 'Chromista'):
+            kingdomName = 'Alga'
+        else:
+            kingdomName = 'Planta'
+        return [(specieId, 'is', kingdomName)]
+
+class SameHabitatRule(InferenceRule):
+    def getqueries(self):
+        partner_enemy = [
+            ('?typeId', 'name', 'Species'),
+            ('?specieId','type','?typeId'),
+            ('?specieId', 'belongs_to', '?familyId'),
+            ('?specieParentId', 'belongs_to', '?familyId')]
+        return [partner_enemy]
+
+    def _maketriples(self, typeId, specieId, familyId, specieParentId):
+        if specieId != specieParentId:
+        return [(specieId, 'parent_of', specieParentId)]
