70dc43a316c469a3de23fd942d1ae9d5e89cc542
unknown <rui.simoes@netvisao.pt>
2015-03-30 01:12:09 +0100

    Update data

--- core/data_parser.py
+++ core/data_parser.py
@@ -35,21 +35,21 @@ for row in reader:
     if row[5] not in species.keys():
         species.update({row[5]: iterator})
         iterator += 1
-    if row[6] not in common.keys():
-        common.update({row[6]: iterator})
-        iterator += 1
-    if row[7] not in state.keys():
-        state.update({row[7]: iterator})
-        iterator += 1
-    if(iterator == 9):
+    # if row[6] not in common.keys():
+    #     common.update({row[6]: iterator})
+    #     iterator += 1
+    # if row[7] not in state.keys():
+    #     state.update({row[7]: iterator})
+    #     iterator += 1
+    if(iterator == 7):
         lista.append(str(kingdom.get(row[0])) + ',name,' + row[0])
         lista.append(str(phylum.get(row[1])) + ',name,' + row[1])
         lista.append(str(classes.get(row[2])) + ',name,' + row[2])
         lista.append(str(order.get(row[3])) + ',name,' + row[3])
         lista.append(str(family.get(row[4])) + ',name,' + row[4])
         lista.append(str(species.get(row[5])) + ',name,' + row[5])
-        lista.append(str(common.get(row[6])) + ',name,' + row[6])
-        lista.append(str(state.get(row[7])) + ',name,' + row[7])
+        # lista.append(str(common.get(row[6])) + ',name,' + row[6])
+        # lista.append(str(state.get(row[7])) + ',name,' + row[7])
     else:
         lista.append(str(kingdom.get(row[0])) + ',name,' + row[0])
         lista.append(str(kingdom.get(row[0])) + ',type,1')
@@ -68,16 +68,16 @@ for row in reader:
         lista.append(str(species.get(row[5])) + ',name,' + row[5])
         lista.append(str(species.get(row[5])) + ',type,6')
         lista.append(str(species.get(row[5])) + ',belongs_to,' + str(family.get(row[4])))
-        lista.append(str(common.get(row[6])) + ',name,' + row[6])
-        lista.append(str(common.get(row[6])) + ',type,7')
-        lista.append(str(common.get(row[6])) + ',belongs_to,' + str(species.get(row[5])))
-        lista.append(str(state.get(row[7])) + ',name,' + row[7])
-        lista.append(str(state.get(row[7])) + ',type,8')
-        lista.append(str(state.get(row[7])) + ',belongs_to,' + str(common.get(row[6])))
+        # lista.append(str(common.get(row[6])) + ',name,' + row[6])
+        # lista.append(str(common.get(row[6])) + ',type,7')
+        # lista.append(str(common.get(row[6])) + ',belongs_to,' + str(species.get(row[5])))
+        # lista.append(str(state.get(row[7])) + ',name,' + row[7])
+        # lista.append(str(state.get(row[7])) + ',type,8')
+        # lista.append(str(state.get(row[7])) + ',belongs_to,' + str(common.get(row[6])))
 
 doc.close()
 
-fp = open("newtriple.csv", "w")
+fp = open("taxon.csv", "w")
 
 for ele in lista:
     fp.write(ele + '\n')
